4c22fec82162bc6570881453fbcf836bce761c38
Rustam Yusupov <yusupov@gnivc.ru>
2022-08-05 00:47:49 +0300

    Initial commit

---
+++ src/test/java/pro/sky/course_work_demo/CourseWorkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course_work_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
